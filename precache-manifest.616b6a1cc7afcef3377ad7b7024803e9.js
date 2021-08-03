ee78cfead0098aa778aedc60b2983753603d38c8
Nico <qtsddxy@163.com>
2021-08-03 14:34:43 +0800

    fs

---
+++ precache-manifest.616b6a1cc7afcef3377ad7b7024803e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb9b01968bc29e92776",
+    "url": "css/app.e7fc69ec.css"
+  },
+  {
+    "revision": "2f5f2ca5e19762f0724e",
+    "url": "css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "5f09b3f50ae06b51e3e4157d26c25663",
+    "url": "index.html"
+  },
+  {
+    "revision": "8eb9b01968bc29e92776",
+    "url": "js/app.f0be92e0.js"
+  },
+  {
+    "revision": "2f5f2ca5e19762f0724e",
+    "url": "js/chunk-vendors.0ad60da6.js"
+  },
+  {
+    "revision": "5df4b4b6e37ce58e9a65ef488ebe9767",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "e628cb34d88b8e336da79d5e7c6506e9",
+    "url": "qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
